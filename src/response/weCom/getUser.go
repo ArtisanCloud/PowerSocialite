7c222f3b5d0f8207f796ecc6e1aef495f49086f2
Matrix-X <matrix-x@artisancloud.com>
2022-08-13 14:55:58 +0800

    feature(wecom): add getUserInfo and getUserDetail

---
+++ src/response/weCom/getUser.go
@@ -0,0 +1,32 @@
+package weCom
+
+import (
+	"github.com/ArtisanCloud/PowerSocialite/v2/src/models"
+)
+
+type ResponseGetUserDetail struct {
+	*ResponseWeCom
+
+	UserID  string `json:"userid"`
+	Gender  string `json:"gender"`
+	Avatar  string `json:"avatar"`
+	QrCode  string `json:"qr_code"`
+	Mobile  string `json:"mobile"`
+	Email   string `json:"email"`
+	BizMail string `json:"biz_mail"`
+	Address string `json:"address"`
+}
+
+type ResponseGetUserInfo struct {
+	*ResponseWeCom
+	DeviceID       string `json:"DeviceId"`
+	UserID         string `json:"UserId"`
+	ExternalUserID string `json:"external_userid"`
+	OpenID         string `json:"OpenId"`
+	UserTicket     string `json:"user_ticket"`
+}
+
+type ResponseGetUserByID struct {
+	*ResponseWeCom
+	*models.Employee
+}
--- src/response/weCom/getUserInfo.go
+++
@@ -1,9 +0,0 @@
-package weCom
-
-type ResponseGetUserInfo struct {
-	*ResponseWeCom
-	DeviceID       string `json:"DeviceId"`
-	UserID         string `json:"UserId"`
-	ExternalUserID string `json:"external_userid"`
-	OpenID         string `json:"OpenId"`
-}
