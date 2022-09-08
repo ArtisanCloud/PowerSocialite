5ba11ef34c3c97ec1ed4e582b38ef2acede69c47
siaoynli <siaoynli@users.noreply.github.com>
2022-09-08 14:01:36 +0800

    增加判断 快照页模式虚拟账号
    增加判断 快照页模式虚拟账号

--- src/response/wechat/base.go
+++ src/response/wechat/base.go
@@ -11,7 +11,9 @@ type ResponseAuthenticatedAccessToken struct {
 	AccessToken  string `json:"access_token"`
 	ExpiresIn    int    `json:"expires_in"`
 	RefreshToken string `json:"refresh_token"`
-	OpenID       string `json:"openid"`
+	OpenID       string `json:"openid,omitempty"`
 	Scope        string `json:"scope"`
-	UnionID      string `json:"unionid"`
+	UnionID      string `json:"unionid,omitempty"`
+	// 是否为快照页模式虚拟账号，只有当用户是快照页模式虚拟账号是返回，值为1
+	IsSnapShotUser int `json:"is_snapshotuser,omitempty"`
 }
